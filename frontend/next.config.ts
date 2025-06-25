b3ca092de3a7bd225162dba6035659a3d5acef6a
Javier Van Breedan <javierdwd@gmail.com>
2025-06-25 10:57:31 -0300

    StyledComponents: Enable it on next-config and add StyledComponentsRegistry

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+
   /* config options here */
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
--- frontend/src/lib/styled-components.tsx
+++ frontend/src/lib/styled-components.tsx
@@ -1,10 +1,38 @@
+'use client';
+
+import React, { useState } from 'react';
+import { useServerInsertedHTML } from 'next/navigation';
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
+
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 
 interface StyledComponentsProviderProps {
   children: React.ReactNode;
 }
+// Reference: https://nextjs.org/docs/app/guides/css-in-js#styled-components
+function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
+  // Only create stylesheet once with lazy initial state
+  // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
+  useServerInsertedHTML(() => {
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    styledComponentsStyleSheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (typeof window !== 'undefined') return <>{children}</>;
+
+  return (
+    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>{children}</StyleSheetManager>
+  );
+}
 
 export function StyledComponentsProvider({ children }: StyledComponentsProviderProps) {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <StyledComponentsRegistry>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </StyledComponentsRegistry>
+  );
 }
